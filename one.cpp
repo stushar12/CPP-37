b7a520740228e52937810d700cb853457af8955e
Tushar Saxena <stushar345@gmail.com>
2021-02-16 15:16:08 +0530

    Update one.cpp

--- one.cpp
+++ one.cpp
@@ -1,47 +1,10 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int main()
+int maxProfit(int prices[] ,int n )
 {
-    int p;
-    cin>>p;
-    while(p--)
+    int profit=0;
+    for(int i=1;i<n;i++)
     {
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++)
-    {
-        cin>>arr[i];
+        if(prices[i]>prices[i-1])
+        profit=profit+(prices[i]-price[i-1]);
     }
-    int i=1;
-    int flag=0;
-    int buy=0,sell=0,profit=0;
-    while(i<n)
-    {
-        if(arr[i]>arr[i-1])
-        {
-            int j=i;
-        buy=j-1;
-        while(i<n && arr[i-1]<arr[i])
-        {
-            i++;
-        }
-        sell=i-1;
-        flag=1;
-        cout<<"("<<buy<<" "<<sell<<")"<<" ";
-        i++;    
-        }
-        else
-        {
-            i++;
-        }   
-        
-
-    }
-    if(flag==0)
-    cout<<"No Profit";
-    cout<<endl;
+    return profit;
 }
-
-}   
